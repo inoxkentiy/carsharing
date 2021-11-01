310e473c5445bbc5421db372a46c617dc7330f24
kesh <92874131+inoxkentiy@users.noreply.github.com>
2021-11-01 21:28:42 +0600

    nemoguopredelitsasdesign

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ let login = (localStorage.getItem("name"));
 let password = (localStorage.getItem("password"));
 
 for (let i = 0; i < 1; i++) {
-    hey.append(`Добро пожаловать,${login}`);
+    hey.append(`Добро пожаловать,${login}.
+    Можете приступать к выбору автомобиля.`);
 }
