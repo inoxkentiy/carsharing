4e71f6f513b7211f9ebe29848216b23d2c7e72d1
kesh <92874131+inoxkentiy@users.noreply.github.com>
2021-11-02 21:08:04 +0600

    оптимизировано

--- regscript.js
+++ regscript.js
@@ -1,4 +1,7 @@
 let btn = document.querySelector("#btn");
+let contacts = document.querySelector("#contacts");
+let stocks = document.querySelector("#stocks");
+
 btn.addEventListener('click', function (st) {
     let email = document.querySelector("#emailInput").value;
     let password = document.querySelector("#passwordInput").value;
@@ -8,4 +11,11 @@ btn.addEventListener('click', function (st) {
         alert("Вы не ввели логин,ошибка")
     }
     window.location.href = 'mainpage.html';
+});
+contacts.addEventListener('click', function () {
+    alert("Здравствуйте,при возникновении каких-либо вопросов или предложений,пожалуйста,свяжитесь с нами по телефону: 88005553535")
+});
+
+stocks.addEventListener('click', function () {
+    alert("Извините,но на данный момент никаких акционных предложений нет.")
 });
