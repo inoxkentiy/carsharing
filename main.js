8c6a66b55bb4d29a538aa56fc94e6079570146a6
kesh <92874131+inoxkentiy@users.noreply.github.com>
2021-11-02 20:10:30 +0600

    более или менее обнов версия

--- main.js
+++ main.js
@@ -6,3 +6,38 @@ for (let i = 0; i < 1; i++) {
     hey.append(`Добро пожаловать,${login}.
     Можете приступать к выбору автомобиля.`);
 }
+
+function app() {
+    const buttons = document.querySelectorAll('.buttn')
+    const cards = document.querySelectorAll('.containersec')
+
+    function filter(category, items) {
+        items.forEach((item) => {
+            const isItemFiltered = !item.classList.contains(category)
+            const isShowAll = category.toLowerCase() === 'all'
+            if (isItemFiltered && !isShowAll) {
+                item.classList.add('anime')
+            } else {
+                item.classList.remove('hide')
+                item.classList.remove('anime')
+            }
+        })
+    }
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const currentCategory = button.dataset.filter
+            filter(currentCategory, cards)
+        })
+    })
+
+    cards.forEach((card) => {
+        card.ontransitionend = function () {
+            if (card.classList.contains('anime')) {
+                card.classList.add('hide')
+            }
+        }
+    })
+}
+
+app()
