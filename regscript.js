d8e0d775023a03705d7d3a2df4c6005b9efd3c6b
mrowa223 <88025828+mrowa223@users.noreply.github.com>
2021-11-03 20:51:05 +0600

    Add files via upload

--- regscript.js
+++ regscript.js
@@ -2,6 +2,7 @@ let btn = document.querySelector("#btn");
 let contacts = document.querySelector("#contacts");
 let stocks = document.querySelector("#stocks");
 
+
 btn.addEventListener('click', function (st) {
     let email = document.querySelector("#emailInput").value;
     let password = document.querySelector("#passwordInput").value;
@@ -10,8 +11,19 @@ btn.addEventListener('click', function (st) {
     if (email === "") {
         alert("Вы не ввели логин,ошибка")
     }
-    window.location.href = 'mainpage.html';
+        let Account={
+         name:email,
+          password:password}
+
+     let arrayAccount=JSON.parse(localStorage.getItem("arrayAccount"));
+    if(arrayAccount===null){
+        arrayAccount=[];
+    }
+    arrayAccount.push(Account);
+    localStorage.setItem("arrayAccount", JSON.stringify(arrayAccount));
+    console.log(arrayAccount)
 });
+
 contacts.addEventListener('click', function () {
     alert("Здравствуйте,при возникновении каких-либо вопросов или предложений,пожалуйста,свяжитесь с нами по телефону: 88005553535")
 });
@@ -19,3 +31,30 @@ contacts.addEventListener('click', function () {
 stocks.addEventListener('click', function () {
     alert("Извините,но на данный момент никаких акционных предложений нет.")
 });
+
+    // Добавление 2-й кнопки.
+    let buttonSecond=document.querySelector("#btnSecond")
+    btnSecond.addEventListener('click', function (st) {
+    let emailSecond = document.querySelector("#emailInput2").value;
+    let passwordSecond = document.querySelector("#passwordInput2").value;
+    localStorage.setItem("name2", (emailSecond));
+    localStorage.setItem("password2", (passwordSecond));
+    //Если email - 0=>alert 
+    if (emailSecond === "") {
+        alert("Вы не ввели логин,ошибка")
+        location.href="registration.html"
+    }
+    if (emailSecond===localStorage.getItem("name") 
+    && passwordSecond===localStorage.getItem("password")
+    )
+     {
+        console.log("Вы успешно вошли")
+        alert("Вы успешно вошли")
+    }
+    location.href="mainpage.html"
+});
+
+let buttonDelete=document.querySelector("#delete")
+    buttonDelete.addEventListener('click',()=>{
+        localStorage.removeItem("arrayAccount");
+    })
