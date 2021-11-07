2e9a944cc7defd4136d1c56cb98f8e2006822df0
mrowa223 <88025828+mrowa223@users.noreply.github.com>
2021-11-07 17:18:12 +0600

    Добавил refresh+код к нему

--- regscript.js
+++ regscript.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector("#btn");
 let contacts = document.querySelector("#contacts");
 let stocks = document.querySelector("#stocks");
+let counter = 0;
 contacts.addEventListener('click', function () {
     alert("Здравствуйте,при возникновении каких-либо вопросов или предложений,пожалуйста,свяжитесь с нами по телефону: 88005553535")
 });
@@ -8,8 +9,8 @@ contacts.addEventListener('click', function () {
 stocks.addEventListener('click', function () {
     alert("Извините,но на данный момент никаких акционных предложений нет.")
 });
-    
-let checkbox=document.querySelector("#checkbox")
+
+let checkbox = document.querySelector("#checkbox")
 
 btn.addEventListener('click', function (st) {
     let email = document.querySelector("#emailInput").value;
@@ -19,74 +20,80 @@ btn.addEventListener('click', function (st) {
     if (email === "") {
         alert("Вы не ввели логин,ошибка")
     }
-        let Account={
-         name:email,
-          password:password}
-
-     let arrayAccount=JSON.parse(localStorage.getItem("arrayAccount"));
-    if(arrayAccount===null){
-        arrayAccount=[];
+    let Account = {
+        name: email,
+        password: password
     }
-    
-    
-    let zxc;
-    zxc=document.getElementById('one');
-        if (zxc.checked) {
-           
-            arrayAccount.push(Account);
-            localStorage.setItem("arrayAccount", JSON.stringify(arrayAccount));
-            console.log(arrayAccount)
-        }
-        else {
-            alert ('Нажмите на квадрат');
-        }
 
-   
-    
-   
-   
-  
+    let arrayAccount = JSON.parse(localStorage.getItem("arrayAccount"));
+    if (arrayAccount === null) {
+        arrayAccount = [];
+    }
+
+    let counter = JSON.parse(localStorage.getItem("counter"));
+    if (counter === null) {
+        counter = [];
+    }
+
+    let zxc;
+    zxc = document.getElementById('one');
+    if (zxc.checked) {
+
+        arrayAccount.push(Account);
+        localStorage.setItem("arrayAccount", JSON.stringify(arrayAccount));
+        console.log(arrayAccount)
+        counter++;
+        localStorage.setItem("counter", JSON.stringify(counter));
+    }
+    else {
+        alert('Нажмите на квадрат');
+    }
+
+
+
+
+
+
 });
 
 
 
-    // Добавление 2-й кнопки.
-    let buttonSecond=document.querySelector("#btnSecond")
-    btnSecond.addEventListener('click', function (st) {
+// Добавление 2-й кнопки.
+let buttonSecond = document.querySelector("#btnSecond")
+btnSecond.addEventListener('click', function (st) {
     let emailSecond = document.querySelector("#emailInput2").value;
     let passwordSecond = document.querySelector("#passwordInput2").value;
     localStorage.setItem("name2", (emailSecond));
     localStorage.setItem("password2", (passwordSecond));
     //Если email - 0=>alert 
-    
-    if (emailSecond===localStorage.getItem("name") && passwordSecond===localStorage.getItem("password")){
+
+    if (emailSecond === localStorage.getItem("name") && passwordSecond === localStorage.getItem("password")) {
         console.log("Вы успешно вошли")
         alert("Вы успешно вошли")
-        location.href="mainpage.html"
+        location.href = "mainpage.html"
     }
-    else{
+    else {
         alert("Вы не ввели логин,ошибка")
-        location.href="registration.html"
+        location.href = "registration.html"
     }
-   
-   
+
+
 });
 
-let buttonDelete=document.querySelector("#delete")
-    buttonDelete.addEventListener('click',()=>{
-        localStorage.removeItem("arrayAccount");
-    })
+let buttonDelete = document.querySelector("#delete")
+buttonDelete.addEventListener('click', () => {
+    localStorage.removeItem("arrayAccount");
+    localStorage.removeItem("counter")
+})
 
 
-  
 
-// function Ex() {
-// let zxc;
-// zxc=document.getElementById('one');
-// 	if (zxc.checked) {
-// 		alert('Квадрат нажат');
-// 	}
-// 	else {
-// 		alert ('Нажмите на квадрат');
-// 	}
-// }
+let refresh = document.querySelector("#refresh")
+
+
+refresh.addEventListener('click', () => {
+    let refreshBlock = $("#refreshBlock")
+
+    refreshBlock.append(`<div>Зарегистрированных  пользователей: ${(localStorage.getItem("counter"))}</div>`)
+    console.log(JSON.parse(localStorage.getItem("counter")))
+})
